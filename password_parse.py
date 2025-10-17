1ec204162cbf2831bfe3cc5963d9f76d6be270ca
bkbuckets14 <bakastens@gmail.com>
2025-10-16 20:15:32 -0400

    File that parses password file

---
+++ password_parse.py
@@ -0,0 +1,16 @@
+def get_database_url(args):
+
+    if args.pwd == None:
+        ROOT_PASSWORD = "admin"
+        DATABASE_NAME = "basketball_db"
+        CONTAINER_NAME = "mysql_container"
+    else:
+        with args.pwd as ff:
+            url_info = ff.readlines()
+            ROOT_PASSWORD = url_info[0].split(":")[1].strip()
+            DATABASE_NAME = url_info[1].split(":")[1].strip()
+            CONTAINER_NAME = url_info[2].split(":")[1].strip()
+
+    DATABASE_URL = "mysql+mysqlconnector://root:"+ROOT_PASSWORD+"@"+CONTAINER_NAME+":3306/"+DATABASE_NAME
+
+    return DATABASE_URL
