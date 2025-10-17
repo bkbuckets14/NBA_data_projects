25c41c5d7c2133a6530a7647e1fd79cf48d8bd3e
bkbuckets14 <bakastens@gmail.com>
2025-10-16 20:15:56 -0400

    File for running queries for shotchart table

--- shotchart_query.py
+++ shotchart_query.py
@@ -1,17 +1,25 @@
+import argparse
+from password_parse import get_database_url
+
 import sqlalchemy as db
 import pandas as pd
 
-DATABASE_URL = "mysql+mysqlconnector://root:admin@mysql_container:3306/basketball_db"
+parser = argparse.ArgumentParser(description="Read text file with database name and password.")
+parser.add_argument("-p", "--pwd", type=argparse.FileType("r"), default=None, help="Path to file that contains container name, database name, and root password.")
+args = parser.parse_args()
+DATABASE_URL = get_database_url(args)
 
 engine = db.create_engine(DATABASE_URL)
 
 conn = engine.connect()
 
-query = db.text('SELECT SHOT_TYPE, COUNT(*) FROM shotchart WHERE PLAYER_ID=1628369 GROUP BY SHOT_TYPE')
+query = db.text('SELECT SHOT_ZONE_RANGE AS sb, COUNT(*) FROM shotchart GROUP BY SHOT_ZONE_RANGE')
 
 exe = conn.execute(query)
 result = exe.fetchall()
 df = pd.DataFrame(data=result)
+lens = [len(word) for word in df.sb]
 print(df)
+print(max(lens))
 
 conn.close()
