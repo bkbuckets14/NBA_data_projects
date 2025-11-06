91a65560769d36db0e8b27fa15256e57d7a0c1ee
bkbuckets14 <bakastens@gmail.com>
2025-11-05 20:09:07 -0500

    Tweaked team_shotchart function to format dates for MySQL insertion and removed entire league shotchart function

--- nba_api_functions.py
+++ nba_api_functions.py
@@ -1,59 +1,66 @@
-from requests.exceptions import ReadTimeout
+import pickle
 
 #nba_api packages
-from nba_api.stats.endpoints import shotchartdetail, leaguegamelog
+from nba_api.stats.endpoints import shotchartdetail, leaguegamelog, teamplayerdashboard
 from nba_api.stats.static import players, teams
+
+from datetime import datetime
 
 import pandas as pd
 
-#function to load the shot records for all players in the 2024-25 season
-def shotchart_2024_2025():
-    #this gets all the players who played in the 2024-25 season and the teams they played for
-    gamelog = leaguegamelog.LeagueGameLog(season='2024-25', player_or_team_abbreviation='P',season_type_all_star='Regular Season')
-    gl_df = gamelog.get_data_frames()[0]
-    player_df = gl_df[['PLAYER_ID', 'TEAM_ID']].drop_duplicates()
-    #this gets the player_ids and team_ids and puts them into a list
-    id_list = [{'player_id': player_id, 'team_id': team_id} for player_id, team_id in zip(player_df.PLAYER_ID, player_df.TEAM_ID)]
-    print("Num Players:", len(id_list)) #to test out how many total players there will be
+def team_shotchart(team, season='2024-25'):
 
-    #this is the list that will hold all the shot data
+    #Load team_dict from pickle file
+    with open("team_dict.pkl", "rb") as ff:
+        team_dict = pickle.load(ff)
+    
+    #get the players from the team for a given season
+    dashboard = teamplayerdashboard.TeamPlayerDashboard(
+        team_id=team_dict[team],
+        per_mode_detailed="Totals",
+        season=season,
+        season_type_all_star="Regular Season",
+        date_from_nullable='10-21-2024'
+    )
+    
+    #get the player_ids
+    player_ids = dashboard.get_data_frames()[1].PLAYER_ID
+    print("Number of Players on", team + ":", player_ids.size)
+
+    #set up list to hold shots for a player and counter to count players
     shot_list = []
+    counter = 0
 
     #for each player_id, team_id pair
-    for ind, id in enumerate(id_list):
-        #tracker to check progress
-        if ind%50 == 0:
-            print('Data Received for', ind, 'players')
+    for player_id in player_ids:
 
-        try:
-            #get the shot data for the player_id, team_id pair
-            shotchart = shotchartdetail.ShotChartDetail(
-                team_id=id['team_id'],
-                player_id=id['player_id'],
+        #get the players shots for the season
+        shotchart = shotchartdetail.ShotChartDetail(
+                team_id=team_dict[team],
+                player_id=player_id,
                 season_type_all_star='Regular Season',
                 season_nullable='2024-25',
                 context_measure_simple='FGA',  # Field Goal Attempts
                 game_id_nullable=''            # leave empty for full season
-            )
+        )
+        
+        #format the results from ShotChartDetail
+        results = shotchart.get_data_frames()[0]
+        results.drop('GRID_TYPE', axis=1, inplace=True)
+        results = results.astype(object)
 
-            #get the data as a dataframe
-            results = shotchart.get_data_frames()[0]
-            results.drop('GRID_TYPE', axis=1, inplace=True)
-            results = results.astype(object)
+        #append each shot to the shot_list
+        for record in results.to_dict(orient='records'):
+            record['GAME_DATE'] = datetime.strptime(record["GAME_DATE"], "%Y%m%d").strftime("%Y-%m-%d") #reformat the date to work with SQL
+            shot_list.append(record)
 
-            #append each shot to the shot_list
-            for record in results.to_dict(orient='records'):
-                shot_list.append(record)
-
-        #if there is an error, print error, player_id, team_id
-        except ReadTimeout as r:
-            print(r)
-            print("Player ID:", id['player_id'])
-            print("Team ID:", id['team_id'])
+        #keep track of players added
+        counter += 1
+        print('Data Received for', counter, 'players')
 
     return shot_list
 
-
+#trial function
 def JB_trial():
     player_id, team_id = get_player_and_team_ids("Jaylen Brown", "Boston Celtics")
 
